f88096bd9d953f20bc2385550fe361e82b102fdf
Sami Ahlfors <hello@samiahlfors.com>
2026-08-26 17:07:27 +0200

    feat: add internal models

--- TwoWaySync/Models/Internal/Company.cs
+++ TwoWaySync/Models/Internal/Company.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace TwoWaySync.Models.Internal
 {
     public class Company
     {
-        
+        public Guid Id { get; set; }
+        public string Contents { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool Completed { get; set; }
+        public DateTime CompletedDate { get; set; }
+        public Guid CompanyId { get; set; }
+        public DateTime ChangedDate { get; set; }
     }
 }
--- TwoWaySync/Models/Internal/Task.cs
+++ TwoWaySync/Models/Internal/Task.cs
@@ -1,7 +1,11 @@
+using System;
+
 namespace TwoWaySync.Models.Internal
 {
     public class Task
     {
-        
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ChangedDate { get; set; }
     }
 }
