80d1a6338b9ba1ec24da4965d89c4db2085bfb42
Sami Ahlfors <hello@samiahlfors.com>
2026-08-26 17:29:29 +0200

    feat: add remote DTOs

--- TwoWaySync/Models/Remote/RemoteCompanyDto.cs
+++ TwoWaySync/Models/Remote/RemoteCompanyDto.cs
@@ -1,7 +1,15 @@
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
 namespace TwoWaySync.Models.Remote
 {
+    // https://stackoverflow.com/a/54540599
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
     public class RemoteCompanyDto
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime LastModifiedDate { get; set; }
     }
 }
--- TwoWaySync/Models/Remote/RemoteTaskDto.cs
+++ TwoWaySync/Models/Remote/RemoteTaskDto.cs
@@ -1,7 +1,19 @@
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
 namespace TwoWaySync.Models.Remote
 {
+    // https://stackoverflow.com/a/54540599
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
     public class RemoteTaskDto
     {
-        
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool Finished { get; set; }
+        public int RelatedCompanyId  { get; set; }
+        public DateTime LastModifiedDate { get; set; }
     }
 }
