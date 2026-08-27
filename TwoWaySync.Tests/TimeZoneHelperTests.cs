f1160dbfe40e2b39091b2e0051777632f8590a42
Sami Ahlfors <hello@samiahlfors.com>
2026-08-27 14:05:09 +0200

    feat: test converting utc to swedish time

--- TwoWaySync.Tests/TimeZoneHelperTests.cs
+++ TwoWaySync.Tests/TimeZoneHelperTests.cs
@@ -6,13 +6,26 @@ namespace TwoWaySync.Tests
 {
     public class TimeZoneHelperTests
     {
-        [Fact]
-        public void ConvertToUtc_ReturnsCetTime()
+        [Theory]
+        [InlineData(2026, 2, 5, 12, 11)]
+        [InlineData(2026, 6, 30, 20, 18)]
+        public void ConvertToUtc_ReturnsUtcTime(int year, int month, int day, int sweHour, int expectedReturnHour)
         {
-            var date = new DateTime(2026, 2, 5, 12, 0, 0, DateTimeKind.Utc);
+            var date = new DateTime(year, month, day, sweHour, 0, 0, DateTimeKind.Unspecified);
             var result = TimeZoneHelper.ConvertToUtc(date);
             
-            Assert.Equal(new DateTime(2026, 2, 5, 11, 0, 0, DateTimeKind.Utc), result);
+            Assert.Equal(new DateTime(year, month, day, expectedReturnHour, 0, 0), result);
+        }
+
+        [Theory]
+        [InlineData(2026, 2, 5, 12, 13)]
+        [InlineData(2026, 6, 30, 16, 18)]
+        public void ConvertToSwedishTime_ReturnsSwedishTime(int year, int month, int day, int utcHour, int expectedReturnHour)
+        {
+            var date = new DateTime(year, month, day, utcHour, 0, 0, DateTimeKind.Utc);
+            var result = TimeZoneHelper.ConvertToSwedishTime(date);
+            
+            Assert.Equal(new DateTime(year, month, day, expectedReturnHour, 0, 0), result);
         }
     }
 }
