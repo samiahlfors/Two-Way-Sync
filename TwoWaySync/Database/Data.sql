ce175a64dff0a05b0a00bed8c4e3f7c7bf3bc82c
Sami Ahlfors <hello@samiahlfors.com>
2026-08-29 17:06:17 +0200

    feat: add basic test data

---
+++ TwoWaySync/Database/Data.sql
@@ -0,0 +1,6 @@
+insert into entity_mapping (entity_type, local_id, remote_id, last_synced)
+values ('Task', '4d9d892d-ae4a-4800-b08e-263328169e0a', 1, now());
+
+insert into entity_mapping (entity_type, local_id, remote_id, last_synced)
+values ('Company', '8f825fd0-c3a2-4000-9941-89a0ba056ee4', 5, now());
+
--- TwoWaySync/Database/Schema.sql
+++ TwoWaySync/Database/Schema.sql
@@ -1,11 +1,14 @@
-create table if not exists entity_mapping(
+drop table if exists entity_mapping;
+drop table if exists sync;
+
+create table entity_mapping(
     entity_type varchar(20) not null,
     local_id uuid not null, 
     remote_id int not null,
     last_synced timestamp not null
 );
 
-create table if not exists sync(
+create table sync(
   entity_type varchar(20) not null,
   last_synced timestamp not null  
 );
