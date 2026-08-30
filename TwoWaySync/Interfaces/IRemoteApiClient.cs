5a2ea29e0a9e01821628596e686b9f6f7b59173a
Sami Ahlfors <hello@samiahlfors.com>
2026-08-30 16:42:02 +0200

    refactor: get last modified as DateTime instead of int

--- TwoWaySync/Interfaces/IRemoteApiClient.cs
+++ TwoWaySync/Interfaces/IRemoteApiClient.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using TwoWaySync.Models.Remote;
 
@@ -6,18 +7,18 @@ namespace TwoWaySync.Interfaces
     public interface IRemoteApiClient
     {
         // GET /tasks/?last_modified_from=N&offset=N&page_size=N
-        List<RemoteTaskDto> GetTasks(int lastModifiedFrom, int offset, int pageSize);
+        List<RemoteTaskDto> GetTasks(DateTime lastModifiedFrom, int offset, int pageSize);
         // GET /tasks/{Id}
-        RemoteTaskDto GetTasks(int id);
+        RemoteTaskDto GetTask(int id);
         // PATCH /tasks/{Id}
         void UpdateTask(int id); // TODO: Also pass in some data here
         // POST /tasks/
         RemoteTaskDto CreateTask(RemoteTaskDto task);
         
         // GET /companies/?last_modified_from=N&offset=N&page_size=N&name=S
-        List<RemoteCompanyDto> GetCompanies(int lastModifiedFrom, int offset, int pageSize, string name);
+        List<RemoteCompanyDto> GetCompanies(DateTime lastModifiedFrom, int offset, int pageSize, string name);
         // GET /companies/{Id}
-        RemoteCompanyDto GetCompanies(int id);
+        RemoteCompanyDto GetCompany(int id);
         // PATCH /companies/{Id}
         void UpdateCompany(int id); // TODO: Also pass in some data here
         // POST /companies/
