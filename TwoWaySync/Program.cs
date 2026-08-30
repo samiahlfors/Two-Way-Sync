f96ec02435da8943ca34e3b197a657014a6f0049
Sami Ahlfors <hello@samiahlfors.com>
2026-08-30 15:00:13 +0200

    chore: cleanup program file

--- TwoWaySync/Program.cs
+++ TwoWaySync/Program.cs
@@ -1,25 +1,10 @@
-using System;
-using System.Net.Http;
-using TwoWaySync.Services;
-
 namespace TwoWaySync
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            var httpClient = new HttpClient { BaseAddress = new Uri("http://localhost:5000") };
-            var apiClient = new RemoteApiClient(httpClient);
             
-            try
-            {
-                var tasks = apiClient.GetTasks(lastModifiedFrom: 0, offset: 0);
-                Console.WriteLine(tasks.Result);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-            }
         }
     }
 }
