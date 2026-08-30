f60f5fde82ac67d5ddcb2c0ff5748868f9759dea
Sami Ahlfors <hello@samiahlfors.com>
2026-08-30 19:00:24 +0200

    feat: instantiate dependencies and invoke sync engine

--- TwoWaySync/Program.cs
+++ TwoWaySync/Program.cs
@@ -1,10 +1,23 @@
+using TwoWaySync.Services;
+
 namespace TwoWaySync
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            // Setup database repository
+            var mappingRepository = new MappingRepository();
             
+            // Setup Remote API Client
+            var remoteApiClient = new RemoteApiClient();
+            
+            // Setup Local API
+            var localApi = new LocalApi();
+            
+            // Run Sync Engine
+            var syncEngine = new SyncEngine(mappingRepository, remoteApiClient, localApi);
+            syncEngine.Run();
         }
     }
 }
