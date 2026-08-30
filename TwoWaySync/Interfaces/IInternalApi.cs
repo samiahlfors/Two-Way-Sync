1daac6f77943766b9ba63711347d7a59a688146c
Sami Ahlfors <hello@samiahlfors.com>
2026-08-30 17:20:01 +0200

    feat: add interface for internal api

---
+++ TwoWaySync/Interfaces/IInternalApi.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using TwoWaySync.Models.Internal;
+
+namespace TwoWaySync.Interfaces
+{
+    public interface IInternalApi
+    {
+        Company GetCompanyByName(string companyName);
+        Company GetCompanyById(Guid companyId);
+        Company CreateCompany(string companyName);
+        List<Company> GetCompanies(DateTime changedFromStart, DateTime changedFromEnd);
+
+        Task GetTaskById(Guid taskId);
+        Task CreateOrUpdateTask(Task task);
+        List<Task> GetTasks(DateTime changedFromStart, DateTime changedFromEnd);
+    }
+}
+
+
+
+/*
+Companies.GetCompanyByName(string) returns Company
+Companies.GetCompanyById(guid) returns Company
+Companies.CreateCompany(string) returns Company
+Companies.GetCompanies(DateTime changedFromStart, DateTime changedFromEnd) returns List<Company>
+
+Tasks.GetTaskById(guid) returns Task
+Tasks.CreateOrUpdateTask(Task) returns Task
+Tasks.GetTasks(DateTime changedFromStart, DateTime changedFromEnd) returns List<Task>
+*/
