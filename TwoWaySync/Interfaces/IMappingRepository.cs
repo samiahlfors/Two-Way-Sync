0201421f9ce29e90780f4c490c0043ece09fb810
Sami Ahlfors <hello@samiahlfors.com>
2026-08-30 18:16:21 +0200

    feat: add comments to mapping repository interface

--- TwoWaySync/Interfaces/IMappingRepository.cs
+++ TwoWaySync/Interfaces/IMappingRepository.cs
@@ -3,11 +3,19 @@ using TwoWaySync.Models;
 
 namespace TwoWaySync.Interfaces
 {
+    // Map local and remote tasks and companies as well as Sync Stamps
     public interface IMappingRepository
     {
+        // Usage: GetEntityByLocalId("Task", new Guid("8f825fd0-c3a2-4000-9941-89a0ba056ee4"))
+        // This should return an EntityMapping object
         EntityMapping GetEntityByLocalId (string entityType, Guid localId);
         EntityMapping GetEntityByRemoteId(string entityType, int remoteId);
+        
+        // This is to save and keep track of mappings
         void SaveMapping();
+        
+        // Sync stamps are used to keep track of specific synchronisations
+        // I.e. local tasks or remote companies
         DateTime GetSyncStamp(string entityType);
         void SaveSyncStamp(string entityType, DateTime syncStamp);
     }
